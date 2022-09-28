5862d9091c515c73a8e664380170095a601bdc72
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2022-09-28 13:38:48 -0700

    Added variables to JavaScript

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+/* variables section */
+var todaysDate = moment().format("LLLL");
+var tasks = [
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+];
