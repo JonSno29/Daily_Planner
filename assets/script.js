5fab35fe91423a83f7f2686e61db483a1dc72ab9
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2022-10-02 14:42:01 -0700

    added timeblocks 10-4pm

--- assets/script.js
+++ assets/script.js
@@ -28,12 +28,67 @@ function timeBlockColor() {
     $("#9am").addClass("future");
   }
 
-if (currentHour > 9) {
-  $("#9am").addClass("past");
-} else if (currentHour >= 9 && currentHour < 9) {
-  $("#9am").addClass("present");
-} else if (currentHour < 9) {
-  $("#9am").addClass("future");
+if (currentHour > 10) {
+  $("#10am").addClass("past");
+} else if (currentHour >= 10 && currentHour < 10) {
+  $("#10am").addClass("present");
+} else if (currentHour < 10) {
+  $("#10am").addClass("future");
 }
 
+if (currentHour > 11) {
+  $("#11am").addClass("past");
+} else if (currentHour >= 11 && currentHour < 12) {
+  $("#11am").addClass("present");
+} else if (currentHour < 11) {
+  $("#11am").addClass("future");
+}
+
+if (currentHour > 12) {
+  $("#12pm").addClass("past");
+} else if (currentHour >= 12 && currentHour < 1) {
+  $("#12pm").addClass("present");
+} else if (currentHour < 12) {
+  $("#12pm").addClass("future");
+}
+
+if (currentHour > 13) {
+  $("#1pm").addClass("past");
+} else if (currentHour >= 13 && currentHour < 14) {
+  $("#1pm").addClass("present");
+} else if (currentHour < 13) {
+  $("#1pm").addClass("future");
+}
+
+if (currentHour > 14) {
+  $("#2pm").addClass("past");
+} else if (currentHour >= 14 && currentHour < 15) {
+  $("#2pm").addClass("present");
+} else if (currentHour < 14) {
+  $("#2pm").addClass("future");
+}
+
+if (currentHour > 15) {
+  $("#3pm").addClass("past");
+} else if (currentHour >= 15 && currentHour < 16) {
+  $("#3pm").addClass("present");
+} else if (currentHour < 15) {
+  $("#3pm").addClass("future");
+}
+
+if (currentHour > 16) {
+  $("#4pm").addClass("past");
+} else if (currentHour >= 16 && currentHour < 17) {
+  $("#4pm").addClass("present");
+} else if (currentHour < 16) {
+  $("#4pm").addClass("future");
+}
+
+if (currentHour > 17) {
+  $("#5pm").addClass("past");
+} else if (currentHour >= 17 && currentHour < 18) {
+  $("#5pm").addClass("present");
+} else if (currentHour < 17) {
+  $("#5pm").addClass("future");
+}
 }
